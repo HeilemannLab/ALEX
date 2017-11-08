a0a1096c6e325344c767b0bc81ee2b6f15de4f35
Karoline Friedl <karoline.friedl@gmx.de>
2017-11-08 14:31:26 +0100

    try

--- timetraces.py
+++ timetraces.py
@@ -29,6 +29,18 @@ def correctRollover(t1, t2):
     return t1, t2
 
 
+def correct2(t):
+    int_32 = (2**32) - 1
+    n = 0
+    dt = t[0, 0]
+    for i in range(1, len(t)):
+        if t[i, 0] < dt:
+            n += 1
+        dt = t[i, 0]
+        t[i, 0] += n * int_32
+    return t
+
+
 def binnedTrace(array):
     trace = []
     n = 1
@@ -45,26 +57,44 @@ def binnedTrace(array):
             bins = 0
     return trace, n - 1
 
+
+def binnedTrace1(array):
+    n_bin = int(np.floor((array[-1] - array[0]) / (1e6)))
+    print(n_bin, array[-1])
+    trace = np.zeros([n_bin, 1], dtype=np.int64)
+    j = 0
+    for i in range(len(trace)):
+        bins = 0
+        t_m = (i + 1) * 1e6
+        while (array[j] < t_m) and (array[j] >= t_m - (1e6)):
+            bins += 1
+            j += 1
+        trace[i] = bins
+    return trace, n_bin
+
+
 filename = str(directory / 'smALEX_APD1.hdf')
 f = tables.open_file(filename, 'r')
-green_trace = np.array(f.root.timestamps[:, :], dtype=np.int64)
+green_trace = np.array(f.root.timestamps[:, :], dtype=np.float)
 f.flush()
 f.close()
 
 filename = str(directory / 'smALEX_APD2.hdf')
 f = tables.open_file(filename, 'r')
-red_trace = np.array(f.root.timestamps[:, :], dtype=np.int64)
+red_trace = np.array(f.root.timestamps[:, :], dtype=np.float)
 f.flush()
 f.close()
 
 green_trace, red_trace = correctRollover(green_trace, red_trace)
+# green_trace = correct2(green_trace)
+# red_trace = correct2(red_trace)
 
-green_trace, num1 = binnedTrace(green_trace[:, 0])
-green_trace[:] = [x / 10 for x in green_trace]
+green_trace, num1 = binnedTrace1(green_trace[:, 0])
+green_trace[:] = [x / 10.0 for x in green_trace]
 num1 = np.arange(num1)
 
-red_trace, num2 = binnedTrace(red_trace[:, 0])
-red_trace[:] = [x / 10 for x in red_trace]
+red_trace, num2 = binnedTrace1(red_trace[:, 0])
+red_trace[:] = [x / 10.0 for x in red_trace]
 num2 = np.arange(num2)
 
 fig = plt.figure()
